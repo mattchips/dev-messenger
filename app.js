262c7b5c0025d7783f513024f8407bc0141b99ff
Adrian Szeto <adrianduongszeto@gmail.com>
2022-06-27 13:06:59 -0700

    updated to server for the application along with adding an api

--- app.js
+++ app.js
@@ -43,24 +43,83 @@ const generatePage = () => {
   `;
 };
 
-//Express server
 
-const express = require('express')
-const app = express()
-const port = 3000
+// Initializing the Javascript SDK
+let client, channel, username, activeUser;
 
-app.get('/', (req, res) => {
-  res.end('Hello World!');
+client = new StreamChat('<STREAM_APP_KEY>');
+```
+> Again, make sure to replace the `<STREAM_APP_KEY>` placeholder with your own key.
+Next, add a function for generating token to the `public/custom.js` file:
+```js
+// [...]
+
+async function generateToken(username) {
+  const { token } = (await axios.get(`/token?username=${username}`)).data;
+  return token;
+}
+
+
+//setting current user for client SDK
+async function initializeClient() {
+  const token = await generateToken(username);
+
+  await client.setUser(
+    {
+      id: username,
+      name: 'The user name', // Update this name dynamically
+      image: 'https://bit.ly/2u9Vc0r' // user image
+    },
+    token
+  ); // token generated from our Node server
+
+  return client;
+}
+
+
+//User to enter messages
+const user = document.getElementById('user-login-input');
+
+user.addEventListener('keyup', function(event) {
+  if (event.key === 'Enter') {
+    checkAuthState();
+  }
 });
 
-app.listen(port, () => {
-    console.log(`app listening at http://localhost:${port}`)
-  });
+checkAuthState();
+
+async function checkAuthState() {
+  if (!user.value) {
+    document.getElementById('login-block').style.display = 'grid';
+    document.getElementsByClassName('chat-container')[0].style.display = 'none';
+  } else {
+    username = user.value;
+
+    await initializeClient();
+
+    document.getElementsByClassName('chat-container')[0].style.display = 'grid';
+    document.getElementById('login-block').style.display = 'none';
+  }
+}
+
+//Express server
+
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.end('Hello World!');
+// });
+
+// app.listen(port, () => {
+//     console.log(`app listening at http://localhost:${port}`)
+//   });
 
 //writing file
 
-fs.writeFile('index.html', generatePage(), err => {
-  if (err) throw err;
+// fs.writeFile('index.html', generatePage(), err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
--- server.js
+++ server.js
@@ -1,16 +1,52 @@
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-  });
-const express = require('express');
-const { appendFile } = require('fs');
-const PORT = process.env.PORT || 3001;
-const ap = express();
+// app.get('/', (req, res) => {
+//     res.json({
+//       message: 'Hello World'
+//     });
+//   });
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
+// const express = require('express');
+// //const { appendFile } = require('fs');
+// const PORT = process.env.PORT || 3000;
+// const app = express();
+
+// // Express middleware
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// // Default response for any other request (Not Found)
+// app.use((req, res) => {
+//   res.status(404).end();
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+const express = require('express');
+const app = express();
+
+// Stream Chat server SDK
+const StreamChat = require('stream-chat').StreamChat;
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.sendFile('/index.html');
+});
+
+app.listen(8800, () => {
+  console.log('Example app listening on port 8800!');
+});
+
+const serverClient = new StreamChat('<STREAM_API_KEY>', '<STREAM_API_SECRET>');
+
+app.get('/token', (req, res) => {
+  const { username } = req.query;
+  if (username) {
+    const token = serverClient.createToken(username);
+    res.status(200).json({ token, status: 'sucess' });
+  } else {
+    res.status(401).json({ message: 'invalid request', status: 'error' });
+  }
+});
