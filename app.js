a151a386f13383f4e405c752eb61a0f6a10afb04
Adrian Szeto <adrianduongszeto@gmail.com>
2022-06-27 14:33:03 -0700

    fixed bug on app.js

--- app.js
+++ app.js
@@ -48,10 +48,10 @@ const generatePage = () => {
 let client, channel, username, activeUser;
 
 client = new StreamChat('<STREAM_APP_KEY>');
-```
-> Again, make sure to replace the `<STREAM_APP_KEY>` placeholder with your own key.
-Next, add a function for generating token to the `public/custom.js` file:
-```js
+
+// > Again, make sure to replace the `<STREAM_APP_KEY>` placeholder with your own key.
+// Next, add a function for generating token to the `public/custom.js` file:
+// ```js
 // [...]
 
 async function generateToken(username) {
