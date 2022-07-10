243edc414ebeca38552525c6f8e1805a9ef39c6c
GianniMejia <101169251+GianniMejia@users.noreply.github.com>
2022-07-10 15:24:34 -0700

    linked stylesheet to index.ejs

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const generatePage = () => {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <link rel="stylesheet" href="./style.css" />
+        <link rel="stylesheet" href="./assets/css/style.css" type="text/css"/>
         <title>Vanilla PHP and JavaScript Stream Group Chat</title>
       </head>
       <body>
@@ -124,7 +124,8 @@ async function checkAuthState() {
     document.getElementById('login-block').style.display = 'none';
   }
 }
-*/
+*/ 
+//Temporary commented rom line 73-127
 
 app.get("/register", function(req, res){
   //render Account creation  page 
